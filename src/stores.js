759e7f91e395a7bb331c9c549fc5a35c20098b63
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-12-30 20:59:03 +0000

    stores now holds auth and db objects to be used universally

--- src/stores.js
+++ src/stores.js
@@ -5,5 +5,7 @@ import { supabase } from './supabaseClient'
 import { Parse } from './classes/parse'
 import bcryptjs from 'bcryptjs';
 
+
 export const db = writable(new DB(supabase))
-export const auth = writable(new Auth(Parse, bcryptjs, supabase))
+console.log()
+export const auth = writable(new Auth(new Parse, bcryptjs, supabase))
