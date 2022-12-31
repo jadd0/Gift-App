c78ba2d130d6b749f11dc193a32479a765c4cc2e
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-12-31 15:56:16 +0000

    new class to be used to encapsulate methods for circles

---
+++ src/classes/circles.ts
@@ -0,0 +1,35 @@
+import { DB } from "./db";
+const supabase = ''
+export class Circles extends DB {
+  constructor() {
+    super(supabase)
+  }
+
+  async createCircle() {
+
+  }
+
+  async deleteCircle() {
+
+  }
+
+  async leaveCircle() {
+
+  }
+
+  async addToCircle() {
+
+  }
+
+  async deleteFromCircle() {
+
+  }
+  
+  async getCircleContent() {
+
+  }
+
+  async getCircleDetails() {
+    
+  }
+}
