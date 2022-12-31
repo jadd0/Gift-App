6f9e1ba0e2e67677832b49200159ac781285382b
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-12-31 16:50:35 +0000

    added uuid maker method

--- src/classes/db.ts
+++ src/classes/db.ts
@@ -7,6 +7,22 @@ export class DB {
 		this.supabase = supabase;
 	}
 
+	UUIDCreator() {
+    var d = new Date().getTime();
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;
+        if(d > 0){
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+  }
+
 	async getAllValues(table: string) {
 		const { data, error } = await this.supabase.from(table).select('*');
 
