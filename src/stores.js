110aa174afb54391313304399aac01f089f89a83
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-12-31 15:57:58 +0000

    changed so now user object store

--- src/stores.js
+++ src/stores.js
@@ -3,9 +3,10 @@ import { DB } from './classes/db'
 import { Auth } from './classes/auth'
 import { supabase } from './supabaseClient'
 import { Parse } from './classes/parse'
+import { User } from './classes/user'
 import bcryptjs from 'bcryptjs';
 
 
 export const db = writable(new DB(supabase))
-console.log()
 export const auth = writable(new Auth(new Parse, bcryptjs, supabase))
+export const user = writable(new User(supabase))
