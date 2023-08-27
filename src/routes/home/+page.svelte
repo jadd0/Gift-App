8db9b9d6b5c3e9c431dc67c646267f3b07479cde
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2023-08-27 14:32:18 +0100

    Snap scrolling

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -35,15 +35,15 @@
 .scroll-container,
 .scroll-area {
   max-width: 850px;
-  height: 600px;
+  height: 89vh;
   font-size: 60px;
 }
 
 .scroll-container {
   overflow: auto;
   -webkit-scroll-snap-type: y mandatory;
-      -ms-scroll-snap-type: y mandatory;
-          scroll-snap-type: y mandatory;
+  -ms-scroll-snap-type: y mandatory;
+	scroll-snap-type: y mandatory;
 }
 
 .scroll-area {
@@ -60,22 +60,6 @@
   align-items: center;
   justify-content: center;
   color: white;
-}
-
-.scroll-area:nth-of-type(1) {
-  background: #49b293;
-}
-
-.scroll-area:nth-of-type(2) {
-  background: #c94e4b;
-}
-
-.scroll-area:nth-of-type(3) {
-  background: #4cc1be;
-}
-
-.scroll-area:nth-of-type(4) {
-  background: #8360A6;
 }
 
 </style>
